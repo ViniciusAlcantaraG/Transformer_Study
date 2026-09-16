248be358b90a6cefbefb9160c03826e85ad47aaa
ViníciusA <vinir570@gmail.com>
2026-09-16 12:18:11 +0200

    Attention and Multi-Head Attention modules

---
+++ attention.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+import math
+
+class Attention(nn.Module):
+
+    def __init__(self, head_dim):
+        super().__init__()
+        self.scale = 1.0/math.sqrt(head_dim)
+
+    def attention(self, query, key, values, mask):
+
+        scores = torch.matmul(query, key.transpose(-2,-1))
+        scores = scores * self.scale
+        scores = scores.masked_fill_(mask, float("-inf"))
+        attention = torch.softmax(scores, dim=1)
+        scores = torch.matmul(scores, values)
+        return scores, attention
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, input_dim, d_model, num_heads):
+        super().__init__()
+        self.input_dim = input_dim
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model//num_heads
+
+        # Compute Q, K and V at once for all heads
+        self.qkv_layer = nn.Linear(input_dim, 3 * d_model)
+        # Final Projection
+        self.linear_layer = nn.Linear(d_model, d_model)
+
+    def forward(self, x, mask=None):
+
+        batch_size, sequence_length, input_dim = x.size()
+
+        # Project x onto q, k, v
+        qkv = self.qkv_layer(x)
+        qkv = qkv.reshape(batch_size, sequence_length, self.num_heads, 3*self.head_dim)
+        # Rearrange to (batch size, num_heads, seq_length, 3*head_dim)
+        qkv = qkv.math.permute(0,2,1,3)
+
+        # Split the last dimension into q, k, v
+        q, k, v = qkv.chunk(3, dim=-1)
+        scaled_dot_product = Attention(self.head_dim)
+        values, attention = scaled_dot_product(q, k, v, mask)
+        values = values.permute(0,2,1,3)
+        values = values.reshape(batch_size, sequence_length, self.num_heads * self.head_dim)
+
+        output = self.linear_layer(values)
+        return output
+
